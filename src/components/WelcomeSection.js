2c2f46e90acddaba666fbd61ace4151f4d29b72d
AngievR05 <159420956+AngievR05@users.noreply.github.com>
2025-03-07 22:37:24 +0200

    flexbox edit

--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -4,6 +4,7 @@ const WelcomeSection = ({ totalBooks }) => {
   return (
     <section className="welcome">
       <h2>Welcome</h2>
+      <div className="welcome-container">
       <div className="welcome-text">
         <p>
           Google Books API allows access to an extensive collection of book
@@ -15,6 +16,7 @@ const WelcomeSection = ({ totalBooks }) => {
       <div className="total-books">
         <h3>Total Books</h3>
         <p>{totalBooks.toLocaleString()} Books</p>
+      </div>
       </div>
     </section>
   );
